14460f7f728ab4d9d7ef3dd4064c4b2038f754c2
Nikodem <cwikla.nikodem@gmail.com>
2023-09-22 16:15:29 +0200

    commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var code;
+
+function createCaptcha() {
+    document.getElementById('captcha').innerHTML = "";
+    var charsArray = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%^&*";
+    var lengthOpt = 6;
+    var captcha = [];
+    for(var i = 0; i < lengthOpt; i++) {
+        var index = Math.floor(Math.random() * charsArray.length + 1);
+        if(captcha.indexOf(charsArray[index]) == -1)
+        captcha.push(charsArray[index]);
+        else i--;
+    }
+    var canv = document.createElement("canvas");
+    canv.id = "captcha";
+    canv.width = 100;
+    canv.height = 50;
+    var ctx = canv.getContext("2d");
+    ctx.font = "25px Georgia";
+    ctx.strokeText(captcha.join(""), 0, 30);
+    code = captcha.join("");
+    document.getElementById("captcha").appendChild(canv);
+}
+
+function validateCaptcha() {
+    event.preventDefault();
+    debugger
+    if(document.getElementById("cpatchaTextBox").value == code) {
+        alert("Valid Captcha")
+    } else {
+        alert("Invalid Captcha. try Again");
+        createCaptcha();
+    }
+}
